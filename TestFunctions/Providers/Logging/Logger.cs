7c9a873640cb8aa5cdc87a324da8353be9e890c5
Siavash Ghassemi <sghassemi@trader.ca>
2017-06-09 14:36:05 -0700

    init commit

---
+++ TestFunctions/Providers/Logging/Logger.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace TestFunctions
+{
+    public class Logger
+    {
+        private Microsoft.Extensions.Logging.ILogger _logger;
+
+        public Logger(Microsoft.Extensions.Logging.ILogger logger)
+        {
+            _logger = logger;
+        }
+
+
+        public void Log(string message)
+        {
+            _logger.LogTrace(message);
+        }
+    }
+}
